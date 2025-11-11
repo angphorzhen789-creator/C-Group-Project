d85e7d47cace28c36be77a4bd29f005e31be6672
angphorzhen789-creator <angphorzhen789@gmail.com>
2025-11-11 20:55:22 +0800

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -154,4 +154,102 @@ void resetPassword() {
 void borrowerManagement(const string& adminID) {
     cout << "\n===== Borrower Management Section =====\n";
     cout << "Welcome, " << adminID << "!\n";
+
+    
+struct Book {
+    string title;
+    string author;
+    string ISBN;
+    int totalCopies;
+    int availableCopies;
+};
+
+
+void addBook(Book books[], int &count, int maxBooks);
+void showAllBooks(Book books[], int count);
+
+int main() {
+    const int MAX_BOOKS = 100;
+    Book books[MAX_BOOKS];
+    int bookCount = 0;
+    int choice;
+
+    do {
+        cout << "\n===== BOOK MANAGEMENT MENU =====\n";
+        cout << "1. Add New Book\n";
+        cout << "2. Show All Books\n";
+        cout << "3. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore(); 
+
+        switch (choice) {
+        case 1:
+            addBook(books, bookCount, MAX_BOOKS);
+            break;
+        case 2:
+            showAllBooks(books, bookCount);
+            break;
+        case 3:
+            cout << "Exiting Book Management Module...\n";
+            break;
+        default:
+            cout << "Invalid choice. Try again.\n";
+        }
+    } while (choice != 3);
+
+    return 0;
+}
+
+
+void addBook(Book books[], int &count, int maxBooks) {
+    if (count >= maxBooks) {
+        cout << "Book list is full!\n";
+        return;
+    }
+
+    Book newBook;
+    cout << "\nEnter Book Title: ";
+    getline(cin, newBook.title);
+    cout << "Enter Author Name: ";
+    getline(cin, newBook.author);
+    cout << "Enter ISBN: ";
+    getline(cin, newBook.ISBN);
+    cout << "Enter Number of Copies: ";
+    cin >> newBook.totalCopies;
+    cin.ignore();
+
+    newBook.availableCopies = newBook.totalCopies;
+    books[count++] = newBook;
+
+    cout << "\nBook added successfully!\n";
+}
+
+
+void showAllBooks(Book books[], int count) {
+    if (count == 0) {
+        cout << "\nNo books in the system yet.\n";
+        return;
+    }
+
+    cout << "\nCurrent Book Inventory:\n";
+    cout << left << setw(30) << "Title" 
+         << setw(20) << "Author" 
+         << setw(15) << "ISBN" 
+         << setw(10) << "Available" 
+         << setw(10) << "Total" << endl;
+
+    cout << string(85, '-') << endl;
+
+    for (int i = 0; i < count; ++i) {
+        cout << left << setw(30) << books[i].title
+             << setw(20) << books[i].author
+             << setw(15) << books[i].ISBN
+             << setw(10) << books[i].availableCopies
+             << setw(10) << books[i].totalCopies
+             << endl;
+    }
+}
+
+
 }
